a6fee28e38fc618cd590515bc71decfba580b63b
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-26 14:08:32 +0100

    Created dropdown using MaterialUI FormControl

--- src/App.js
+++ src/App.js
@@ -1,24 +1,31 @@
-import React from 'react';
-import logo from './logo.svg';
-import './App.css';
+import React, { useState } from "react";
+import { MenuItem, FormControl, Select } from "@material-ui/core";
+import "./App.css";
 
 function App() {
+  const [countries, setCountries] = useState([]);
+
+  const endPoint = "https://disease.sh/v3/covid-19/countries";
+
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="app">
+      <div className="app__header">
+        <FormControl className="app__dropdown">
+          <Select variant="outlined" value="abc">
+            {countries.map((country) => (
+              <MenuItem value={country}>{country}</MenuItem>
+            ))}
+          </Select>
+        </FormControl>
+      </div>
+      {/* Header */}
+      {/* Title */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* InfoBoxes */}
+      {/* Table */}
+      {/* Graph */}
+      {/* Map */}
     </div>
   );
 }
