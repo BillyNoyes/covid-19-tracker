027679fcb744db422e2bb74113e5c841a39225a2
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-26 15:55:00 +0100

    Started work on LineGraph component

--- src/LineGraph.js
+++ src/LineGraph.js
@@ -1,11 +1,60 @@
-import React from "react";
+import React, { useState, useEffect } from "react";
 import "./LineGraph.css";
+import { Line } from "react-chartjs-2";
 
-function LineGraph() {
+function LineGraph({ casesType }) {
+  const [data, setData] = useState({});
+
+  const endPoint = "https://disease.sh/v3/covid-19/historical/all?lastdays=120";
+
+  const buildChartData = (data, casesType) => {
+    let chartData = [];
+    let lastDataPoint;
+    for (let date in data.cases) {
+      if (lastDataPoint) {
+        let newDataPoint = {
+          x: date,
+          y: data[casesType][date] - lastDataPoint,
+        };
+        chartData.push(newDataPoint);
+      }
+      lastDataPoint = data[casesType][date];
+    }
+    return chartData;
+  };
+
+  useEffect(() => {
+    const fetchData = async () => {
+      await fetch(endPoint)
+        .then((response) => {
+          return response.json();
+        })
+        .then((data) => {
+          let chartData = buildChartData(data, casesType);
+          setData(chartData);
+          // buildChart(chartData);
+        });
+    };
+
+    fetchData();
+  }, [casesType]);
+
   return (
-    <div className="lineGraph">
-      {/* Hello */}
-      <h1></h1>
+    <div>
+      {data?.length > 0 && (
+        <Line
+          data={{
+            datasets: [
+              {
+                backgroundColor: "rgba(204, 16, 52, 0.5)",
+                borderColor: "#CC1034",
+                data: data,
+              },
+            ],
+          }}
+          // options={options}
+        />
+      )}
     </div>
   );
 }
