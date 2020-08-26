c1baea3505efa1a8211a251abf2403cf2938667d
William (Billy) Noyes <wbillypn@gmail.com>
2020-08-26 14:43:53 +0100

    Select country from dropdown and replace value

--- src/App.js
+++ src/App.js
@@ -4,6 +4,7 @@ import "./App.css";
 
 function App() {
   const [countries, setCountries] = useState([]);
+  const [country, setCountry] = useState("worldwide");
 
   const endPoint = "https://disease.sh/v3/covid-19/countries";
 
@@ -22,11 +23,18 @@ function App() {
     getCountriesData();
   }, [countries]);
 
+  const onCountryChange = async (event) => {
+    const countryCode = event.target.value;
+    setCountry(countryCode);
+  };
+
   return (
     <div className="app">
       <div className="app__header">
+        <h1>COVID-19 TRACKER</h1>
         <FormControl className="app__dropdown">
-          <Select variant="outlined" value="abc">
+          <Select variant="outlined" value={country} onChange={onCountryChange}>
+            <MenuItem value="worldwide">Worldwide</MenuItem>
             {countries.map((country) => (
               <MenuItem value={country.value}>{country.name}</MenuItem>
             ))}
